8c5e37e29c6c6e6976967d2e05e7aaff1c315dcb
Goldan32 <50270724+Goldan32@users.noreply.github.com>
2025-06-27 18:38:35 +0200

    Hyprland installer

---
+++ hyprland/install.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+. "$(dirname "${BASH_SOURCE[0]}")"/../utils.sh
+
+if which hyprland > /dev/null; then
+    log "Success (already done)"
+    exit 0
+fi
+
+cd "$HOME" || exit 1
+git clone -b 24.04 --depth=1  https://github.com/JaKooLit/Ubuntu-Hyprland.git ~/Ubuntu-Hyprland-24.04
+cd "$HOME/Ubuntu-Hyprland-24.04" || exit 2
+chmod +x install.sh
+./install.sh
+
+log "Success"
+exit 0
--- hyprlock/install.sh
+++ hyprlock/install.sh
@@ -2,11 +2,6 @@
 
 . "$(dirname "${BASH_SOURCE[0]}")"/../utils.sh
 
-if ! which hyprlock > /dev/null; then
-    log "Failed (use hyprland install script)"
-    exit 1
-fi
-
 if [ -f /etc/pam.d/hyprlock ]; then
     log "Success (already done)"
     exit 0
