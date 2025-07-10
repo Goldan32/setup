fb4b48b436437969f2ce9ce3a4776c2718414fc1
Goldan32 <50270724+Goldan32@users.noreply.github.com>
2025-07-10 11:22:27 +0200

    Much headache

--- main.py
+++ main.py
@@ -1,3 +1,19 @@
 #!/usr/bin/env /usr/bin/python3
 
+from os import path as op
+import sh
+
+root_dir = op.dirname(op.abspath(__file__))
+
+asd = map(lambda x: x.strip(), sh.ls("-1", f"{root_dir}"))
+
+def selectDirs(x):
+    if op.isdir(op.join(root_dir, x.strip())):
+        return True
+    return False
+
+items = filter(selectDirs, asd)
+
+for i, item in enumerate(items):
+    print(f"{i}: {item}")
 
